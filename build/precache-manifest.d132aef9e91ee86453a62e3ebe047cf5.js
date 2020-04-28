679a702f55b3637d8a7729288a88e4f3a12177b8
Han Wang <hyw2@cornell.edu>
2020-04-28 01:20:55 -0400

    Add build folder to github

---
+++ build/precache-manifest.d132aef9e91ee86453a62e3ebe047cf5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa63dad57de71949130523921ef78f10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d66b5ac0968abbf20edb",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "fd916fa7144b435f691c",
+    "url": "/static/css/main.94b71aaf.chunk.css"
+  },
+  {
+    "revision": "d66b5ac0968abbf20edb",
+    "url": "/static/js/2.671fd85f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.671fd85f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd916fa7144b435f691c",
+    "url": "/static/js/main.375aaedb.chunk.js"
+  },
+  {
+    "revision": "c3090e369a8586c2779d",
+    "url": "/static/js/runtime-main.640a5175.js"
+  },
+  {
+    "revision": "99fb497fad685239c31a5c499a561310",
+    "url": "/static/media/header-background.99fb497f.png"
+  },
+  {
+    "revision": "ad4e644ac0808931be19714936cd4d38",
+    "url": "/static/media/pic1.ad4e644a.png"
+  },
+  {
+    "revision": "0f83c9109395a9dfe7d8435c9935b6b7",
+    "url": "/static/media/pic2.0f83c910.png"
+  }
+]);
